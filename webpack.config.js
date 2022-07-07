6fb1ebc405a5da9d3e225117ced3f44c6656aa29
jiangbo <ma021024@126.com>
2022-07-07 12:49:17 +0800

    打包上传

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  optimization: {
+    runtimeChunk: 'single',
+   splitChunks: {
+     cacheGroups: {
+       vendor: {
+         test: /[\\/]node_modules[\\/]/,
+         name: 'vendors',
+         chunks: 'all',
+       },
+     },
+   },
+  },
   plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
   module: {
     rules: [
